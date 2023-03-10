0681a6abad0015fc2e6cd913c389f7fcd6805732
ktnjdev <kthomasnj@gmail.com>
2023-03-10 14:26:48 -0500

    Attempting to correct push issues.

--- assets/script/script.js
+++ assets/script/script.js
@@ -54,6 +54,8 @@ var loseCounter = 0;
 var isWin = false;
 var timer;
 var timerCount;
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
 
 document.querySelector("#count").textContent = localStorage.getItem("count");
 
@@ -480,3 +482,44 @@ function resetGame() {
 }
 
 resetButton.addEventListener("click", resetGame);
+
+// Speed Reader
+
+var message =
+  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
+var words = message.split(' ');
+
+function countdown() {
+  var timeLeft = 5;
+
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
+
+function displayMessage() {
+  var wordCount = 0;
+
+  var msgInterval = setInterval(function () {
+    if (words[wordCount] === undefined) {
+      clearInterval(msgInterval);
+
+    } else {
+      mainEl.textContent = words[wordCount];
+      wordCount++;
+    }
+  }, 1000);
+}
+
+countdown();
